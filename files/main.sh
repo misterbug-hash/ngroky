34ac8511a4fcc8eb6d733befa15c227057bf6915
misterbug-hash <78592977+misterbug-hash@users.noreply.github.com>
2021-02-08 18:14:47 +0530

    Update main.sh

--- files/main.sh
+++ files/main.sh
@@ -1,16 +1,21 @@
 #!/data/data/com.termux/files/usr/bin/bash
-echo -e '\e[101m :::::::::MENU:::::::::: \e[91 .'
-echo '                          '
-echo 'please select your Client Server'
-echo -e '\e[92m -{1}- http server'
-echo -e '\e[92m -{2}- Tcp server'
-echo '                              '
-read -p 'Enter your choice :' choice
-if [[ $choice == 1 || $choice == 01 ]]; then
 clear
-read -p "Enter any 4 digit port no. :" port
+figlet __NGROK__
+
+echo -e '                 \e[101m ::::.MENU.::::\e[0m'
+echo '            '
+echo -e '        \e[101m please select your Client Server\>
+echo '                              '
+echo '                __________________      '
+echo -e '              \e[92m |-{1}- http server|'
+echo -e '              \e[92m |-{2}- Tcp server |'
+echo '
+            '
+read -p '               Enter your choice :' choice
+if [[ $choice == 1 || $choice == 01 ]]; then
+read -p "              Enter port no. :" port
 ./ngrok http $port
 elif [[ $choice == 2 || $choice == 02 ]]; then
-read -p "enter any 4 digit port no. :" porte
+read -p "              Enter port no. :" porte
 ./ngrok tcp $porte
 fi
